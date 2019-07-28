5aecaae66f8cd62ff7188d70ed69019126b4d305
Jason Viola <jason.viola@mavs.uta.edu>
2019-07-28 15:46:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(people)
+  phrase = "The line is currently: "
+  if people.length == 0
+    puts "The line is currently empty."
+  else 
+    people.each do |name|
+    phrase.push(name.index += 1. )
+    phrase.push(name)
+    end
+  end
+end
